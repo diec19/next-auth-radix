5c60d18c5e7f0f583cd692253881415fa92c5be4
DiegoC <diec.cas19@gmail.com>
2024-02-08 10:01:10 -0300

    first commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@radix-ui/themes"
+import { BookmarkIcon } from "@radix-ui/react-icons"
+
+function LoginPage() {
+  return (
+    <div>
+      LoginPage
+    
+    <Button>
+      <BookmarkIcon width='16' height='16'/> Login
+    </Button>
+    </div> 
+  )
+}
+
+export default LoginPage
