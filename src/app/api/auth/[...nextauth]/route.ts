9d13046dcc0dee7f27a9224812c91b699191cdbb
DiegoC <diec.cas19@gmail.com>
2024-02-15 12:47:11 -0300

    next auth added

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+    providers:[
+        CredentialsProvider({
+            name:"credentials",
+            credentials:{
+                email:{
+                    label:"Email",
+                    type:"email",
+                    placeholder:"user@something.com",
+                
+                },
+                password: {
+                    label: "Password",
+                    type: "pasword",               
+                },
+            },
+            async authorize(){
+                return null;
+                
+            }
+        })
+    ],
+    pages:{
+       signIn:"/auth/login",         
+    }
+
+})
+
+export { handler as GET, handler as POST}
